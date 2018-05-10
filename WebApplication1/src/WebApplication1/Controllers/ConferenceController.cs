fb9e21d41a1a46cbe499f84b312406c17b0205bc
nitinverma123 <nitin.verma.in@gmail.com>
2018-05-10 15:41:04 +0530

    Initial Commit
    Initial Commit

---
+++ WebApplication1/src/WebApplication1/Controllers/ConferenceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        IEmployee _employee;
+        public ConferenceController(IEmployee employee)
+        {
+            this._employee = employee;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            ViewBag.Title = "Conferenece";
+            return View( await _employee.GetEmployee(id));
+        }
+    }
+}
---
+++ WebApplication1/src/WebApplication1/Entity/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+
+    public interface IEmployee
+    {
+        Task<Employee> GetEmployee(int Id);
+        void AddEmployee(Employee employee);
+
+
+    }
+    public class Employee : IEmployee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        void IEmployee.AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<Employee> IEmployee.GetEmployee(int Id)
+        {
+            //return  await new Employee() { ID = Id, Name = "Nitin" };   
+            Employee emp = new Employee();
+            await Task.Run(() => {
+                emp.ID = Id; emp.Name = "Niitn"; });
+            return emp;
+        }
+    }
+}
